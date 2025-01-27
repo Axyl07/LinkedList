b1177f7d6125214ff57b90cac0713c45c5681734
Axyl07 <aryanchauhan000777@gmail.com>
2025-01-27 16:18:33 +0530

    append done

---
+++ src/LinkedList.js
@@ -0,0 +1,29 @@
+
+ class LinkedList{
+  // constructor(list) {
+  //     this.list = list;
+  // }
+     head = new Node();
+  append(value) {
+      let temp = this.head;
+      while (temp.nextNode != null) temp = temp.nextNode;
+      const newNode = new Node(value, null);
+      temp.nextNode = newNode;
+}
+}
+
+
+ class Node{
+  value = null;
+  nextNode = null;
+  constructor(value, nextNode) {
+      this.value = value;
+      this.nextNode = nextNode;
+  }
+}
+
+export {LinkedList,Node}
+// const n1 = new Node('a');
+// const n2 = new Node('b', n1);
+// console.log(n1);
+// console.log(n2);
--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
-import test from "./test";
-test();
+import { LinkedList } from "./LinkedList";
+
+
+
+const list = new LinkedList();
+list.append("dog");
+list.append("cat");
+list.append("parrot");
+console.log(list);
+console.log(list.toString());
