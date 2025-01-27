82e5967c5fa370d32bd9da056e02370095bbad74
Axyl07 <aryanchauhan000777@gmail.com>
2025-01-27 19:23:43 +0530

    All tasks done

--- src/LinkedList.js
+++ src/LinkedList.js
@@ -1,53 +1,133 @@
+class LinkedList {
+  head = null;
+  append(value) {
+    if (this.head == null) this.head = new Node(value, null);
+    else {
+      let temp = this.head;
+      while (temp.nextNode != null) temp = temp.nextNode;
+      temp.nextNode = new Node(value, null);
+    }
+  }
+  prepend(value) {
+    if (this.head == null) {
+      this.head = new Node(value, null);
+    } else {
+      let newNode = new Node(value, this.head);
+      this.head = newNode;
+    }
+  }
 
- class LinkedList{
-     head = null;
-     append(value) {
-         if (this.head == null) this.head = new Node(value, null);  
-         else {
-             let temp = this.head;
-             while (temp.nextNode != null) temp = temp.nextNode;
-             temp.nextNode = new Node(value,null);
-        }       
-         }
-     prepend(value) {
-         if (this.head==null) {
-             this.head = new Node(value, null);  
-         } else {
-             let newNode = new Node(value, this.head);
-             this.head = newNode;
-         }
-     }
- 
-     size() {
-     if (this.head === null) {
-         return 0;
-     } else {
-         let length = 1
-         let temp = this.head;
-         while (temp.nextNode != null) {
-             length++;
-             temp = temp.nextNode;
-         }
-         return length;
-     }
-     }
-     Head() {
-         return this.head;
-     }
-}
+  size() {
+    if (this.head === null) {
+      return 0;
+    } else {
+      let length = 1;
+      let temp = this.head;
+      while (temp.nextNode != null) {
+        temp = temp.nextNode;
+        length++;
+      }
+      return length;
+    }
+  }
+  Head() {
+    return this.head;
+  }
+  tail() {
+    let temp = this.head;
+    while (temp.nextNode != null) temp = temp.nextNode;
+    return temp;
+  }
+  at(index) {
+    let i = 0;
+    let temp = this.head;
+    while (temp.nextNode != null) {
+      i++;
+      if (i === index) {
+        return temp;
+      }
+      temp = temp.nextNode;
+    }
+    return temp;
+  }
+  pop() {
+    let temp = this.head;
+    while (temp.nextNode.nextNode != null) temp = temp.nextNode;
+    temp.nextNode = null;
+  }
 
-
- class Node{
-  value = null;
-  nextNode = null;
-  constructor(value, nextNode) {
-      this.value = value;
-      this.nextNode = nextNode;
+  contains(value) {
+    let temp = this.head;
+    while (temp.nextNode != null) {
+      if (temp.value === value) {
+        return true;
+      }
+      temp = temp.nextNode;
+    }
+    if (temp.value === value) {
+      return true;
+    } else return false;
+  }
+  find(value) {
+    let i = 0;
+    let temp = this.head;
+    while (temp.nextNode != null) {
+      i++;
+      if (temp.value === value) {
+        return i;
+      }
+      temp = temp.nextNode;
+    }
+    return null;
+  }
+  toString() {
+    let temp = this.head;
+    let string = "";
+    while (temp.nextNode != null) {
+      string += ` ( ${temp.value} ) -> `;
+      temp = temp.nextNode;
+    }
+    string += ` ( ${temp.value} ) -> null `;
+    return string;
+  }
+  insertAt(value, index) {
+    let i = 0;
+    let temp = this.head;
+    while (temp.nextNode != null) {
+      i++;
+      if (i === index) {
+        const newNode = new Node(value, temp.nextNode);
+        temp.nextNode = newNode;
+      }
+      temp = temp.nextNode;
+    }
+    if (i === index) {
+      const newNode = new Node(value, temp.nextNode);
+      temp.nextNode = newNode;
+    }
+  }
+  removeAt(index) {
+    let i = 0;
+    let prevNode = this.head;
+    let currentNode = this.head.nextNode;
+    while (currentNode.nextNode != null) {
+      i++;
+      if (i === index) {
+        prevNode.nextNode = currentNode.nextNode;
+      }
+      prevNode = prevNode.nextNode;
+      currentNode = currentNode.nextNode;
+    }
   }
 }
 
-export {LinkedList,Node}
-// const n1 = new Node('a');
-// const n2 = new Node('b', n1);
-// console.log(n1);
-// console.log(n2);
+class Node {
+  value = null;
+  nextNode = null;
+  constructor(value, nextNode) {
+    this.value = value;
+    this.nextNode = nextNode;
+  }
+}
+
+export { LinkedList, Node };
--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
 import { LinkedList } from "./LinkedList";
-
-
+console.log(
+  "Note:Use append and Prepend to add elements at start/end of list instead of insertAt/removeAt",
+);
 
 const list = new LinkedList();
+list.prepend("dinosaur");
+list.prepend("trex");
+
 list.append("dog");
 list.append("cat");
 list.append("parrot");
-list.prepend("dinosaur");
-list.prepend("dinosaur0");
-list.append("panda")
+list.append("hamster");
+list.append("snake");
+list.append("turtle");
+list.insertAt("pant", 4);
+list.removeAt(3);
+
+list.insertAt("zebra", 3);
+list.insertAt("hero", 5);
+list.removeAt(5);
+list.pop();
+list.pop();
 console.log(list);
 console.log(list.size());
-console.log(list.Head())
+console.log(list.Head());
+console.log(list.tail());
+console.log(list.at(2));
+console.log(list.contains("dinosaur0"));
+console.log(list.find("cat"));
 console.log(list.toString());
